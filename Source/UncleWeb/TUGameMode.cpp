61b49745a5e953ff5f9f57a22e8415447702ef5e
JeongJongMun <whdans4005@naver.com>
2025-05-02 20:14:21 +0900

    chore: 사용하지 않는 GameMode 코드 삭제

--- Source/UncleWeb/TUGameMode.cpp
+++ Source/UncleWeb/TUGameMode.cpp
@@ -6,17 +6,5 @@
 
 ATUGameMode::ATUGameMode()
 {
-	// Use Blueprint
-	// static ConstructorHelpers::FClassFinder<APawn> CharacterPlayer(TEXT("/Game/Characters/BP_TUCharacterPlayer.BP_TUCharacterPlayer_C"));
-	// if (CharacterPlayer.Succeeded())
-	// {
-	// 	DefaultPawnClass = CharacterPlayer.Class;
-	// }
-	// else
-	// {
-	// 	UE_LOG(LogTemp, Warning, TEXT("[%s()] Failed Load DefaultPawnClass"), *FString(__FUNCTION__));
-	// }
-
-	// Use C++
 	DefaultPawnClass = ATUCharacterPlayer::StaticClass();
 }
